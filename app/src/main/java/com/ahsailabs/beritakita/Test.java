d13e951879d9cf1dcf1bafe164e7df4a8fb9a481
Ahmad Saifullah <ahsai001@gmail.com>
2021-01-17 10:20:43 +0700

    add Test class

---
+++ app/src/main/java/com/ahsailabs/beritakita/Test.java
@@ -0,0 +1,10 @@
+package com.ahsailabs.beritakita;
+
+/**
+ * Created by ahmad s on 17/01/21.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello AppMaker");
+    }
+}
