851dbc3a0729ed0b7d1a7d59f9ebe95aded773d8
Ahmad Saifullah <ahsai001@gmail.com>
2021-01-26 22:28:16 +0700

    update

--- app/src/main/java/com/ahsailabs/beritakita/bases/BaseApp.java
+++ app/src/main/java/com/ahsailabs/beritakita/bases/BaseApp.java
@@ -1,19 +1,28 @@
 package com.ahsailabs.beritakita.bases;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
+
+import androidx.multidex.MultiDex;
 
 /**
  * Created by ahmad s on 09/09/20.
  */
 public class BaseApp extends Application {
     public String data = "ini data di BaseApp";
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         Log.d("BaseApp", "oncreate");
-
         //inisialisasi beberap library
 
         //
